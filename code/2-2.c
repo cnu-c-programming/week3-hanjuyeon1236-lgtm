d34d7cb6bebfaa4e4aa880b8ec6410543aed50fe
hanjuyeon1236-lgtm <hanjuyeon1236@gmail.com>
2026-03-24 16:40:17 +0900

    Update 2-2.c

---
+++ code/2-2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void swap(int *x, int *y) {
+
+}
+
+int main() {
+  int x = 100, y = 200;
+
+  printf("x = %d, y = %d\n", x, y);
+
+  swap();
+  printf("x = %d, y = %d\n", x, y);
+
+  reurn 0;
+}

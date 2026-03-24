5b2f506e668f3294a7453037924470a49432b9a3
hanjuyeon1236-lgtm <hanjuyeon1236@gmail.com>
2026-03-24 16:43:43 +0900

    Update 2-3.c

---
+++ code/2-3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void divide(int a, int b, int *q, int *r) {
+
+}
+
+int main() {
+  int a = 27;
+  int b = 4;
+  int q = 0;
+  int r = 0;
+
+  divide();
+
+  prinf("%d\n%d\n", q, r);
+
+  reurn 0;
+}

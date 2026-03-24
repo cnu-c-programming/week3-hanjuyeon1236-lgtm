604a49e5110c9588db1e44dbe083319393b30907
hanjuyeon1236-lgtm <hanjuyeon1236@gmail.com>
2026-03-24 16:29:49 +0900

    Update 1-3.c

---
+++ code/1-3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int power(int base, int exp) {
+
+}
+
+int main() {
+  printf("%d\n", power(2, 3));
+  printf("%d\n", power(5, 0));
+  printf("%d\n", power(3, 4));
+
+return 0;
+}

044d258d1f8de49fc10aaaeb57e67bf60f11dd1b
hanjuyeon1236-lgtm <hanjuyeon1236@gmail.com>
2026-03-26 02:45:59 +0900

    Update 3-4.c

--- code/3-4.c
+++ code/3-4.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+int max_of(int count, ...) {
+  va_list ap;
+  int max_val;
+
+  va_start(ap, count);
+  max_val = va_arg(ap, int);
+
+  for(int i = 1; i < count; i++) {
+  int current = va_arg(ap, int);
+  if(current > max_val) {
+      max_val = current;
+  }
+  }
+  va_end(ap);
+  return max_val;
+}
 int main() {
   printf("%d\n", max_of(4, 5, 12, 3, 9));
   printf("%d\n", max_of(6, 1, 2, 3, 4, 5, 6));
